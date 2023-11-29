452446d5309e9930363359e25c004be46352466b
Adam Ross <14985050+R055A@users.noreply.github.com>
2023-11-29 17:06:52 +0100

    Env Wrapper with Dispatch Rules (#27)
    * Basic custom env with agent training graph callback
    WIP
    
    Merge complete
    
    * Update custom environment
    WIP
    
    Merge with main
    
    * Update custom environment
    WIP
    
    Merge with main
    
    * Init custom environment wrapper
    WIP
    
    Merge with main
    
    * Add black formatting and ruff lint
    
    * Fix GitHub actions workflow
    
    * Fix check_env workflow and code convention in all files
    
    * Update custom environment
    WIP
    
    * Revert action type annotation change
    
    * Fix check env workflow
    
    * Placeholder
    
    * manual_agent
    
    Manual agent added
    
    * Update custom environment
    
    * Fix Job model and factory datetimes
    
    * Refactor formatting using black lib
    
    * Added arrival time to job class
    
    * Implementing dispatch rules on env
    
    * Starting working on an env wrapper
    
    * Started working on my environment wrapper
    
    * Updated rules to work with new env
    
    * Refactor dispatch rules env wrapper
    
    Co-authored-by: Derrick Adjei <adjeiderrick12@gmail.com>
    
    * Fix efficiency graphing for dispatch rules
    Refactor format using black lib
    
    * Refactor format using ruff lib
    
    * Add code env check for dispatch rule env wrapper to CI workflow
    
    * Fix dispatch rule env wrapper to pass env check
    
    * Refactor format using black lib
    
    ---------
    
    Co-authored-by: nicoviola91 <nico_viola91@hotmail.com>
    Co-authored-by: Derrick Adjei <adjeiderrick12@gmail.com>

--- custom_environment/machine.py
+++ custom_environment/machine.py
@@ -128,12 +128,24 @@ class Machine:
             if recipe.get_recipe_type() in self.__valid_recipe_types
         ]
 
+    def can_perform_job(self, job: Job) -> bool:
+        for recipe in job.get_pending_recipes():
+            if recipe in self.__valid_recipe_types:
+                return True
+
+        return False
+
     def assign_job(self, job_to_assign: Job) -> bool:
         available_valid_recipes: list[Recipe] = self.get_job_valid_recipes(
             job=job_to_assign
         )
 
+        print(
+            f"For job {job_to_assign.get_factory_id()} machine availa: {self.__is_available} recipe: {len(available_valid_recipes)}"
+        )
+
         if available_valid_recipes and self.__is_available:
+            print(f"Can assign job to machine {self.get_id()}")
             next_valid_recipe_to_process: Recipe = available_valid_recipes[0]
             is_recipe_assigned: bool = job_to_assign.set_recipe_in_progress(
                 next_valid_recipe_to_process
@@ -160,6 +172,7 @@ class Machine:
             f" \nStatus: {self.is_available_str()}"
             f"\nCurrent time spent idle: {self.__time_idle}"
             f"\nCurrent time spend active: {self.__time_active}"
+            f"\nValid recipes: {self.__valid_recipe_types}"
             f"\nWorking on recipe(s): "
             f"{[job.get_recipes_in_progress() for job in self.__active_jobs]}"
             f" for the following Job(s): {self.__active_jobs}"
